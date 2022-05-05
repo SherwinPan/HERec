3c5c006e42f1bd7ff13ac3fd8517cf1bab268480
Sherwin <panxw007@gmail.com>
2022-05-06 01:16:53 +0800

    person页面建设，电影查找，影人查找功能

---
+++ src/pages/searchMovie.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "searchMovie"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/searchPerson.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "searchPerson"
+}
+</script>
+
+<style scoped>
+
+</style>
